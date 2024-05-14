e87a18b3b334e3549c8022368c7756553193a8f2
kranthi1346 <46166536+kranthi1346@users.noreply.github.com>
2024-05-14 14:31:13 +0530

    Update CustomImage.tsx

--- src/app/components/CustomImage.tsx
+++ src/app/components/CustomImage.tsx
@@ -1,13 +1,51 @@
-import { cx } from "@emotion/css";
 import { isEmpty } from "lodash";
 import React from "react";
 
-export const CustomImage = ({ name = "", ...props }) => {
-  const images = {
-    backArrow: { src: "path_of_image/svg", alt: "alt_name" },
+interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  name?: string;
+}
 
-    //add images and svgs here
+interface ImageData {
+  [key: string]: { src: string; alt: string };
+}
+
+const CustomImage: React.FC<ImageProps> = ({ name = "", ...props }) => {
+  const images: ImageData = {
+    tickWhiteIcon: {
+      src: "icons/svg/tick_white_icon.svg",
+      alt: "tick_white_icon",
+    },
+    crossGrayIcon: {
+      src: "icons/svg/cross_gray_icon.svg",
+      alt: "cross_gray_icon",
+    },
+    crossRedIcon: {
+      src: "icons/svg/cross_red_icon.svg",
+      alt: "cross_red_icon",
+    },
+    squareAddOrangeIcon: {
+      src: "icons/svg/square_add_orange_icon.svg",
+      alt: "square_add_orange_icon",
+    },
+    warningOrangeIcon: {
+      src: "icons/svg/warning_orange_icon.svg",
+      alt: "warning_orange_icon",
+    },
+    crossBlackIcon: {
+      src: "icons/svg/cross_black_icon.svg",
+      alt: "cross_black_icon",
+    },
+    sucessIcon: {
+      src: "icons/svg/sucess_icon.svg",
+      alt: "sucess_icon",
+    },
+    errorIcon: {
+      src: "icons/svg/error_icon.svg",
+      alt: "error_icon",
+    },
+    // Add more images and svgs here
   };
+
   const imageSrc = name && images[name]?.src ? images[name]?.src : props.src;
   const imageAlt = name && images[name]?.alt ? images[name]?.alt : props.alt;
 
@@ -19,3 +57,5 @@ export const CustomImage = ({ name = "", ...props }) => {
     />
   );
 };
+
+export default CustomImage;
